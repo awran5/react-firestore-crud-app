83c63500a39448c5bb90d15fdfca7464fc0d1d85
awran5 <awran5@yahoo.com>
2020-10-15 07:32:36 +0200

    remove unnecessary import

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,7 +4,6 @@ import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import Card from "react-bootstrap/Card"
-import Button from "react-bootstrap/Button"
 
 const Layout = ({ children }) => {
   return (
